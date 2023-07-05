7b703ffa0ed6f8e0a37c19777b373eb3b948446f
viltho <abdullahabuhammed@gmail.com>
2023-07-05 22:56:20 +0300

    new_updates on api: v2

--- movies/urls.py
+++ movies/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import MoviesList, MoviesDelete, MoviesUpdate, MoviesCreate
+from .views import MoviesList, MoviesUpdate, FavoriteList, FavoriteDetail
 
 urlpatterns = [
     path('',MoviesList.as_view(), name='movielist'),
-    path('delete/<int:pk>/',MoviesDelete.as_view(), name='moviedelete'),
     path('update/<int:pk>/',MoviesUpdate.as_view(), name='movieupdate'),
-    path('create/',MoviesCreate.as_view(), name='moviecreate'),
+    path('favorites/',FavoriteList.as_view(), name='favoritelist'),
+    path('favorites/<int:pk>',FavoriteDetail.as_view(), name='favoritelist'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,24 +1,40 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import Movies 
-from .serializers import MovieSerializer
+from .models import Movies, Favorite
+from .serializers import MovieSerializer, FavoriteSerializer
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from .permissions import IsOwnerOrReadOnly
 
 # Create your views here.
 
 # ListAPIView
-class MoviesList(generics.ListCreateAPIView):
+class MoviesList(generics.ListAPIView):
     queryset = Movies.objects.all()
     serializer_class = MovieSerializer
+    permission_classes = [IsAuthenticated]
+    
+# class MoviesDelete(generics.DestroyAPIView):
+#     queryset = Movies.objects.all()
+#     serializer_class = MovieSerializer
+#     permission_classes = [IsOwnerOrReadOnly]
+    
+class MoviesUpdate(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movies.objects.all()
+    serializer_class = MovieSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    
+# class MoviesCreate(generics.CreateAPIView):
+#     queryset = Movies.objects.all()
+#     serializer_class = MovieSerializer
+#     permission_classes = [IsAuthenticated]
+    
+class FavoriteList(generics.ListCreateAPIView):
+    queryset = Favorite.objects.all()
+    serializer_class = FavoriteSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    
+class FavoriteDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Favorite.objects.all()
+    serializer_class = FavoriteSerializer
+    permission_classes = [IsOwnerOrReadOnly]
 
-# RetrieveAPIView RetrieveUpdateAPIView
-class MoviesDelete(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Movies.objects.all()
-    serializer_class = MovieSerializer
-    
-class MoviesUpdate(generics.RetrieveUpdateAPIView):
-    queryset = Movies.objects.all()
-    serializer_class = MovieSerializer
-    
-class MoviesCreate(generics.CreateAPIView):
-    queryset = Movies.objects.all()
-    serializer_class = MovieSerializer
